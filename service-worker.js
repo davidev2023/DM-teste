477e2390373bac09bd329013c27853f1a176a6ad
davidev2023 <133832913+davidev2023@users.noreply.github.com>
2026-07-23 14:13:06 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,45 @@
 const cacheName = "dm-financeira-v10";
 
 const arquivos = [
-"./",
-"./index.html",
-"./style.css",
-"./app.js"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
 ];
 
+// Instalação do Service Worker
 self.addEventListener("install", evento => {
-
-evento.waitUntil(  
+  // Força o Service Worker novo a se tornar ativo imediatamente
+  self.skipWaiting();
+  
+  evento.waitUntil(  
     caches.open(cacheName)  
-    .then(cache => cache.addAll(arquivos))  
-);
-
+      .then(cache => cache.addAll(arquivos))  
+  );
 });
 
+// Ativação e Limpeza de Caches Antigos (v1, v2, etc.)
+self.addEventListener("activate", evento => {
+  evento.waitUntil(
+    caches.keys().then(chaves => {
+      return Promise.all(
+        chaves.map(chave => {
+          if (chave !== cacheName) {
+            return caches.delete(chave); // Deleta caches antigos
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Assume o controle da página imediatamente
+  );
+});
+
+// Interceptador de Requisições (offline)
 self.addEventListener("fetch", evento => {
-
-evento.respondWith(  
-
+  evento.respondWith(  
     caches.match(evento.request)  
-    .then(resposta => {  
-
+      .then(resposta => {  
         return resposta || fetch(evento.request);  
-
-    })  
-
-);
-
+      })  
+  );
 });
